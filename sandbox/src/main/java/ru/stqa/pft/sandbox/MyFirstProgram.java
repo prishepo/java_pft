a33dea9d987e6484bb7d1bcd5beb1ee4ca6cbd11
prishepo <124888106+prishepo@users.noreply.github.com>
2023-02-11 00:32:16 +0300

    Задание 2. Расстояние между точками (main class)

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,31 +1,30 @@
 package ru.stqa.pft.sandbox;
 
+import java.io.PipedOutputStream;
+
 public class MyFirstProgram {
     public static void main(String[] args) {
 
-        hello("world");
-        hello("user");
-        hello("Alexei");
+        /*Point p1 = new Point();
+        p1.x = 10;
+        p1.y = 5;
 
+        Point p2 = new Point();
+        p2.x = 15;
+        p2.y = 30;*/
 
-        double l = 5;
-        System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+        /*Point p1 = new Point(10,5);
+        Point p2 = new Point(15,30);*/
 
-        double a = 4;
-        double b = 6;
-        System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
+        Point p = new Point(10,5,15,30);
+
+        System.out.println("Расстояние между точками p1 и p2 = " + p.distance());
 
     }
+/*
+    public static double distance(Point p1, Point p2) {
+        return Math.sqrt((Math.pow((p2.x - p1.x), 2) + Math.pow((p2.y - p1.y), 2)));
+    }*/
 
-    public static void hello(String somebody) {
-        System.out.println("Hello " + somebody + "!");
-    }
 
-    public static double area(double len) {
-        return len * len;
-    }
-
-    public static double area (double a, double b) {
-        return a*b;
-    }
 }
