d6fe05a194dc94ede7b439d5ec6a47486b54a174
prishepo <124888106+prishepo@users.noreply.github.com>
2023-02-17 23:30:44 +0300

    Задание 3

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/GroupCreationTests.java
@@ -0,0 +1,75 @@
+package ru.stqa.pft.addressbook;
+
+import java.time.Duration;
+import java.util.regex.Pattern;
+import java.util.concurrent.TimeUnit;
+import org.testng.annotations.*;
+import static org.testng.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import java.io.File;
+
+
+public class GroupCreationTests {
+  private WebDriver driver;
+
+
+  @BeforeClass(alwaysRun = true)
+  public void setUp() throws Exception {
+    System.setProperty("webdriver.chrome.driver", "");
+    driver = new ChromeDriver();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+
+  }
+
+  @Test
+  public void testGroupCreationTests() throws Exception {
+    driver.get("http://localhost/addressbook/");
+    driver.findElement(By.name("user")).clear();
+    driver.findElement(By.name("user")).sendKeys("admin");
+    driver.findElement(By.name("pass")).click();
+    driver.findElement(By.name("pass")).clear();
+    driver.findElement(By.name("pass")).sendKeys("secret");
+    driver.findElement(By.xpath("//input[@value='Login']")).click();
+    driver.findElement(By.linkText("groups")).click();
+    driver.findElement(By.name("new")).click();
+    driver.findElement(By.name("group_name")).click();
+    driver.findElement(By.name("group_name")).clear();
+    driver.findElement(By.name("group_name")).sendKeys("test1");
+    driver.findElement(By.name("group_header")).click();
+    driver.findElement(By.name("group_header")).clear();
+    driver.findElement(By.name("group_header")).sendKeys("test2");
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).click();
+    driver.findElement(By.name("group_footer")).clear();
+    driver.findElement(By.name("group_footer")).sendKeys("test3");
+    driver.findElement(By.name("submit")).click();
+    driver.findElement(By.linkText("group page")).click();
+    driver.findElement(By.linkText("Logout")).click();
+  }
+
+  @AfterClass(alwaysRun = true)
+  public void tearDown() throws Exception {
+    driver.quit();
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+}
