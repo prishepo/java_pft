748251b06a02031d7bc9bf318622c657472bd9f1
prishepo <124888106+prishepo@users.noreply.github.com>
2023-02-25 18:38:27 +0300

    Реализована проверка и обеспечение выполнения предусловий

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/GroupDeletionTests.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,12 +1,17 @@
 package ru.stqu.pft.addressbook.tests;
 
 import org.testng.annotations.Test;
+import ru.stqu.pft.addressbook.model.GroupData;
 
 public class GroupDeletionTests extends TestBase{
 
   @Test
   public void testGroupDeletion() throws Exception {
     app.getNavigationHelper().gotoGroupPage();
+    if (!app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().createGroup(new GroupData("test1", null, null));
+    }
+
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().deleteSelectedGroups();
     app.getGroupHelper().returnToGroupPage();
