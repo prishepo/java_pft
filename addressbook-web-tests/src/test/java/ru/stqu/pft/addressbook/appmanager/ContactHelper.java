ccb8f27dadc23d2974dfd446edbc4a8087f5a4db
prishepo <124888106+prishepo@users.noreply.github.com>
2023-03-02 11:21:10 +0300

    Задание 8. Корректировка метода isThereAGroupInContactCreationForm

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/appmanager/ContactHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/appmanager/ContactHelper.java
@@ -75,6 +75,7 @@ public class ContactHelper extends HelperBase {
     }
 
     public boolean isThereAGroupInContactCreationForm(String group) {
+        gotoContactCreationPage();
         return isElementPresent(By.xpath("//select[@name='new_group']//option[text()='"+group + "']"));
 
     }
