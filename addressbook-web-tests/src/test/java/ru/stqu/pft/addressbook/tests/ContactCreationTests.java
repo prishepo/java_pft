e3909765dac2fe11036cc8fd7984c8ee238eefbf
prishepo <124888106+prishepo@users.noreply.github.com>
2023-03-17 23:24:54 +0300

    Загрузка тестовых данных из файла для контактов

--- addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/ContactCreationTests.java
+++ addressbook-web-tests/src/test/java/ru/stqu/pft/addressbook/tests/ContactCreationTests.java
@@ -7,7 +7,10 @@ import ru.stqu.pft.addressbook.model.ContactData;
 import ru.stqu.pft.addressbook.model.Contacts;
 import ru.stqu.pft.addressbook.model.GroupData;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -19,22 +22,32 @@ import static org.hamcrest.MatcherAssert.*;
 public class ContactCreationTests extends TestBase {
 
     @BeforeMethod
-    public void ensurePreconditions() {
-        if (!app.contact().isThereAGroupInContactCreationForm("test1")) {
-            app.goTo().groupPage();
-            app.group().create(new GroupData().withName("test1"));
+    public void ensurePreconditions() throws IOException {
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null){
+            String[] split = line.split(";");
+                if (!app.contact().isThereAGroupInContactCreationForm(split[7])) {
+                    app.goTo().groupPage();
+                    app.group().create(new GroupData().withName(split[7]));
+                }
+                line = reader.readLine();
         }
+
     }
 
     @DataProvider
-    public Iterator<Object[]> validContacts() {
+    public Iterator<Object[]> validContacts() throws IOException {
         List<Object[]> list = new ArrayList<Object[]>();
-        list.add(new Object[]{new ContactData().withFirstName("Sergey").withMiddleName("Petrovich").withLastName("Ivanov").withCompanyName("Burger King").
-                withAddress("Moscow, Tushinskaya st, 17").withMobilePhone("+71234567890").withEmail("ivanov1981@yandex123.ru").withGroup("test1")});
-        list.add(new Object[]{new ContactData().withFirstName("Fedor").withMiddleName("Alexandrovich").withLastName("Sidorov").withCompanyName("KFC").
-                withAddress("Moscow, Tverskaya st, 22").withMobilePhone("+7262344").withEmail("Sidorov@mail.ru").withGroup("test2")});
-        list.add(new Object[]{new ContactData().withFirstName("Nikolay").withMiddleName("Mihaylovich").withLastName("Semenov").withCompanyName("Mcdonalds").
-                withAddress("Tula, Isacovskovo st, 16a").withMobilePhone("+788888888").withEmail("ivanov1981@yandex123.ru").withGroup("test3")});
+        BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.csv")));
+        String line = reader.readLine();
+        while (line != null) {
+            String[] split = line.split(";");
+            list.add(new Object[] {new ContactData().withFirstName(split[0]).withMiddleName(split[1]).withLastName(split[2]).withCompanyName(split[3]).
+                    withAddress(split[4]).withMobilePhone(split[5]).withEmail(split[6]).withGroup(split[7])});
+            line = reader.readLine();
+        }
+
         return list.iterator();
     }
 
