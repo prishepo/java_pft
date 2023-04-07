7927a12609456ff39fd8a41031f7e9a7d241206c
prishepo <124888106+prishepo@users.noreply.github.com>
2023-04-07 23:48:40 +0300

    Реализация передачи файлов на удаленную машину

--- mantis-tests/out/test/resources/config_inc.php
+++ mantis-tests/out/test/resources/config_inc.php
@@ -1,7 +1,12 @@
 <?php
-$g_hostname = 'localhost';
-$g_db_type = 'mysql';
-$g_database_name = 'bugtracker';
-$g_db_username = 'root';
-$g_db_password = '';
+$g_hostname               = 'localhost';
+$g_db_type                = 'mysqli';
+$g_database_name          = 'bugtracker';
+$g_db_username            = 'root';
+$g_db_password            = '';
+
+$g_default_timezone       = 'Europe/Berlin';
+
+$g_crypto_master_salt     = '4eVEHy05Gj9GjRQUBmKgaBmEfCEnDcCZFlGgTbDnyi4=';
+
 $g_signup_use_captcha = OFF;
