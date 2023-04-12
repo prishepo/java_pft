4508430b8a12d8357dc21fe2c8697b21686665c5
prishepo <124888106+prishepo@users.noreply.github.com>
2023-04-12 15:14:20 +0300

    доработки 18 задание

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ChangePasswordTest.java
@@ -1,11 +1,14 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
+import ru.stqa.pft.mantis.model.UserData;
 
 import javax.mail.MessagingException;
-import javax.mail.Session;
 import java.io.IOException;
+import java.util.List;
 
 import static org.testng.Assert.assertTrue;
 import static ru.stqa.pft.mantis.tests.TestBase.app;
@@ -13,6 +16,7 @@ import static ru.stqa.pft.mantis.tests.TestBase.app;
 
 
 public class ChangePasswordTest extends TestBase {
+    private SessionFactory sessionFactory;
 
     @Test
     public void testChangePassword() throws IOException, MessagingException {
@@ -21,7 +25,10 @@ public class ChangePasswordTest extends TestBase {
         app.adminActions().goToManagePage();
         app.adminActions().goToContactsManagePage();
         System.out.println(app.db().users());
-        app.adminActions().selectUser();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+        List<UserData> result = session.createQuery("from UserData").list();
+        System.out.println(result);
 
 
 
